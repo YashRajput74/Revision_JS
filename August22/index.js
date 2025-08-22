cd770e32db8ff648c63ce6af485025962914c428
Yash Rajput <yashrajput7455@gmail.com>
2025-08-22 19:15:12 +0530

    binary search

---
+++ August22/index.js
@@ -0,0 +1,31 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function binSearch(arr, target) {
+    let flag = true;
+    let lowest = 0;
+    let highest = arr.length;
+    let index = -1;
+    while (flag) {
+        let midPt = Math.floor((lowest + highest) / 2);
+        if (lowest > highest || lowest == highest) {
+            console.log("lowest is greater than highest")
+            flag = false;
+        }
+        else if (arr[midPt] > target) {
+            highest = midPt;
+        }
+        else if (arr[midPt] < target) {
+            lowest = midPt;
+        }
+        else if (arr[midPt] == target) {
+            flag = false;
+            index = midPt;
+        }
+        else {
+            console.log("broken")
+            flag = false;
+        }
+    }
+    return index;
+}
+console.log(binSearch(arr, 2))
