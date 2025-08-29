397e4f64972b5134d3441465e48571b357a11a9e
Yash Rajput <yashrajput7455@gmail.com>
2025-08-29 19:04:37 +0530

    map proto practice

---
+++ August29/index.js
@@ -0,0 +1,8 @@
+Array.prototype.myMap = function (callbackFn) {
+    const newArr = [];
+    for(let i=0;i<this.length;i++){
+        let returnedVal=callbackFn(this[i],i,this);
+        returnedVal.push(newArr);
+    }
+    return newArr;
+}
