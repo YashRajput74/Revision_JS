464c191a3a79f0f18517a9b50bfa5f6f1bc36c3b
Yash Rajput <yashrajput7455@gmail.com>
2026-01-12 23:53:00 +0530

    remove the atsk

--- Jan9/reactPractice/src/App.jsx
+++ Jan9/reactPractice/src/App.jsx
@@ -7,7 +7,6 @@ Debounced API call (300ms) //debouncing??useEffect??
 
 Show loading state //We can do that
 
-Cancel previous request when typing fast //“If a request is already in flight and the user types again, abort it.”
 */
 
 export default function App(){
